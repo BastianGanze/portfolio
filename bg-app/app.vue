455762f36cd00107cf7dcc8ab332813599ba5891
BastianGanze <bastianganze@gmail.com>
2025-04-04 11:14:15 +0200

    fix build

--- bg-app/app.vue
+++ bg-app/app.vue
@@ -2,7 +2,7 @@
   <div>
     <button class="btn">Button</button>
     <h1 class="text-3xl font-bold underline">
-      Try something different.
+      Try something different again.
     </h1>
   </div>
 </template>
