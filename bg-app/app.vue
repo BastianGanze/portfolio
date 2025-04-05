abdf4efd3d0e7c9842a093b3b760312f516a6b0d
BastianGanze <bastianganze@gmail.com>
2025-04-05 16:14:26 +0200

    add pre-commit hook to bump version

--- bg-app/app.vue
+++ bg-app/app.vue
@@ -1,10 +1,14 @@
+<script setup lang="ts">
+import { version } from './package.json'
+</script>
+
 <template>
   <div>
     <button class="btn">
       Button
     </button>
     <h1 class="text-3xl font-bold underline">
-      Try something different again.
+      Try something different again. {{ version }}
     </h1>
   </div>
 </template>
