ad6ceb6ab93d5e8cc0609a449351a9cdec9e21e6
BastianGanze <bastianganze@gmail.com>
2025-04-05 15:48:19 +0200

    add some meta data

--- bg-app/nuxt.config.ts
+++ bg-app/nuxt.config.ts
@@ -1,10 +1,20 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      title: 'Bastian Ganze',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+  css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
   },
